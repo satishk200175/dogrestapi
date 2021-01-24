3c24aeae2b6ced2b5c6edeed07f4fb0755cbdbf2
satishkumar <satishk20@outlook.com>
2021-01-23 19:46:58 -0500

    initial

---
+++ src/main/java/com/udacity/dogrestapi/service/DogService.java
@@ -0,0 +1,12 @@
+package com.udacity.dogrestapi.service;
+
+import com.udacity.dogrestapi.entity.Dog;
+
+import java.util.List;
+
+public interface DogService {
+    List<Dog> retrieveDogs();
+    List<String> retrieveDogBreed();
+    String retrieveDogBreedById(Long id);
+    List<String> retrieveDogNames();
+}
